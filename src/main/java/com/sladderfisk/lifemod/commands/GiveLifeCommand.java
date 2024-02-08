b0fdf030af2a8d6af488f71ae538b2a3d2413116
Sladderfisk <august.stretmo@elev.ga.lbs.se>
2024-02-08 22:17:33 +0100

    Givelife command is complete!!! (may do some tweking in the future)

--- src/main/java/com/sladderfisk/lifemod/commands/GiveLifeCommand.java
+++ src/main/java/com/sladderfisk/lifemod/commands/GiveLifeCommand.java
@@ -8,8 +8,14 @@ import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.commands.arguments.MessageArgument;
+import net.minecraft.network.chat.ChatType;
 import net.minecraft.network.chat.OutgoingChatMessage;
+import net.minecraft.network.chat.PlayerChatMessage;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.scores.Objective;
+import net.minecraft.world.scores.ScoreAccess;
+import net.minecraft.world.scores.ScoreHolder;
+import net.minecraft.world.scores.Scoreboard;
 
 public class GiveLifeCommand {
 
@@ -22,13 +28,35 @@ public class GiveLifeCommand {
             ServerPlayer player = EntityArgument.getPlayer(command, "target");
 
             if (player == null) return 1;
+            //if (player == command.getSource().getPlayer()) return 0;
+
+            SetLife(player, 1);
+
+            WhisperToPlayer(command.getSource().getPlayer(), "Test");
+
+            SetLife(command.getSource().getPlayer(), -1);
+
 
 
             return 0;
         })));
     }
 
-    private static void sendMessage(ServerPlayer target){
-        //OutgoingChatMessage message = OutgoingChatMessage.create()
+    private static void SetLife(ServerPlayer player, int val){
+
+        Scoreboard scoreboard = player.getScoreboard();
+        Objective objective = scoreboard.getObjective("Lives");
+        ScoreAccess score = scoreboard.getOrCreatePlayerScore(player, objective);
+        score.set(score.get() + val);
+    }
+
+    private static void WhisperToPlayer(ServerPlayer player, String mes){
+
+
+        OutgoingChatMessage message = OutgoingChatMessage.create(PlayerChatMessage.system(mes));
+        ChatType.Bound type = ChatType.bind(ChatType.MSG_COMMAND_INCOMING, player);
+
+        player.sendChatMessage(message, true, type);
+        player.sendChatMessage(message, false, type);
     }
 }
